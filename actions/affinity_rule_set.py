679a2499ad26ec6fa76f8e2c23d9d652e3fa54c6
Bradley Bishop <bradley.bishop@encore.tech>
2019-01-14 14:21:33 -0500

    fixed issues with the affinity rule create

--- actions/affinity_rule_set.py
+++ actions/affinity_rule_set.py
@@ -1,13 +1,51 @@
 # Disable message for: No name 'vim' in module 'pyVmomi' (no-name-in-module)
+from vmwarelib.actions import BaseAction
+from vmwarelib import inventory
 from pyVmomi import vim  # pylint: disable-msg=no-name-in-module
-from vmware_base import VmwareBaseAction
 import time
 
 
-class VmwareAffinityRuleSet(VmwareBaseAction):
+class VmwareAffinityRuleSet(BaseAction):
 
-    def __init__(self, config):
-        super(VmwareAffinityRuleSet, self).__init__(config)
+    def run(self, vm_names, vm_wait_retry, rule_name, cluster_name, host_names=None, vsphere=None):
+        """Main entry point for the StackStorm actions to execute the operation.
+        :returns: boolean
+        """
+        # Connect to the vCenter
+        self.establish_connection(vsphere)
+
+        # Find the vm to add to the affinity rule
+        vms = []
+        for vm_name in vm_names:
+            vms.append(self.wait_for_vm(vm_name, vm_wait_retry))
+
+        hosts = []
+        if host_names:
+            for host_name in host_names:
+                hosts.append(inventory.get_hostsystem(self.si_content, name=host_name))
+        else:
+            for vm in vms:
+                # Get the host that the vm is current on
+                hosts.append(vm.runtime.host)
+
+        # Finds cluster by name
+        cluster = inventory.get_cluster(self.si_content, name=cluster_name)
+
+        # Create vmWare Affinity groups
+        vm_group_name, vm_group_spec = self.create_group(rule_name, vms, "vm")
+        host_group_name, host_group_spec = self.create_group(rule_name, hosts, "host")
+
+        # Create vmWare Affinity rules
+        affinity_rule_spec = self.create_rule(rule_name, vm_group_name, host_group_name)
+
+        # Create cluster change config to be passed to vmware
+        config_spec = vim.cluster.ConfigSpecEx(groupSpec=[vm_group_spec, host_group_spec],
+                                              rulesSpec=[affinity_rule_spec])
+
+        # Run Reconfigure task
+        task_return = self._wait_for_task(cluster.ReconfigureEx(config_spec, modify=True))
+
+        return task_return
 
     def create_group(self, rule_name, object_array, group_type):
         """ Creates a group to be created in VMware. Returns the group
@@ -61,43 +99,3 @@ class VmwareAffinityRuleSet(VmwareBaseAction):
             raise ValueError("Could not find Virtual Machine with name: {0}".format(vm_name))
 
         return vm
-
-    def run(self, vm_names, vm_wait_retry, rule_name, cluster_name, host_names=None, vsphere=None):
-        """Main entry point for the StackStorm actions to execute the operation.
-        :returns: boolean
-        """
-        # Connect to the vCenter
-        self.establish_connection(vsphere)
-
-        # Find the vm to add to the affinity rule
-        vms = []
-        for vm_name in vm_names:
-            vms.append(self.wait_for_vm(vm_name, vm_wait_retry))
-
-        hosts = []
-        if host_names:
-            for host_name in host_names:
-                hosts.append(inventory.get_hostsystem(self.si_content, name=host_name))
-        else:
-            for vm in vms:
-                # Get the host that the vm is current on
-                hosts.append(vm.runtime.host)
-
-        # Finds cluster by name
-        cluster = inventory.get_cluster(self.si_content, name=cluster_name)
-
-        # Create vmWare Affinity groups
-        vm_group_name, vm_group_spec = self.create_group(rule_name, vms, "vm")
-        host_group_name, host_group_spec = self.create_group(rule_name, hosts, "host")
-
-        # Create vmWare Affinity rules
-        affinity_rule_spec = self.create_rule(rule_name, vm_group_name, host_group_name)
-
-        # Create cluster change config to be passed to vmware
-        config_spec = vim.cluster.ConfigSpecEx(groupSpec=[vm_group_spec, host_group_spec],
-                                              rulesSpec=[affinity_rule_spec])
-
-        # Run Reconfigure task
-        task_return = self._wait_for_task(cluster.ReconfigureEx(config_spec, modify=True))
-
-        return task_return
